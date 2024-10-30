b67e55154ef797114cc0afb9be70b1c09e4424bf
Andrei Starichenko <centroid@mail.ru>
2024-10-30 15:40:15 +0300

    [bus_user] add

--- PROJECT.py
+++ PROJECT.py
@@ -42,13 +42,34 @@ class PROJECT(PROJECT_BASE):
     # PROJECT ----------------------------------------------
     NAME_IMPORT: str = "base_aux"
     KEYWORDS: list[str] = [
-        "funcs_aux",
     ]
 
     # README -----------------------------------------------
     # add DOUBLE SPACE at the end of all lines! for correct representation in MD-viewers
     DESCRIPTION_SHORT: str = "collect all previous moduls in one package"
     DESCRIPTION_LONG: str = """
+    
+    bus_user
+    ###
+NOTE: IT SEEMS THIS IS OLD DATA! see tests for actual usage!
+    
+!. MOST APPROPRIATE COMMAND PROTOCOL
+other protocols mot recommended
+
+1. all cmds must be as params (preferred) in equipment or as special command
+2. [<CMD_NAME>] - read param value or run special command  
+    [IDN] - read value IDN  
+    [DUMP] - run special command 
+3. [<CMD_NAME> <VALUE>] - write value in parameter or run special cmd with param  
+    [VOUT 12.3] - set value into parameter VOUT  
+4. [<CMD_NAME> ?] - get available values to write into parameter  
+    [MODE ?] - return [0 1 2 3]
+5. all command sent must return answer  
+    [OK] - if no value was asked
+    [<VALUE>] - if asked some value, returned without measurement unit
+    [FAIL] - any common not specified error
+    [FAIL 0123] - any specified error without description
+    [FAIL 02 VALUE OUT OF RANGE] - any specified error with description (full variant)
     
 """
     FEATURES: list[str] = [
@@ -131,6 +152,28 @@ class PROJECT(PROJECT_BASE):
          "[FastApi] (preferred)",
          ],
         "client_requests item+stack",
+
+        # bus_user
+        ["[SerialClient]",
+         "keep all found ports in base class!",
+         ],
+        ["Serial",
+         "Client+Server",
+         "connect with Type__AddressAutoAcceptVariant FIRST_FREE/FIRST_FREE__ANSWER_VALID",
+         "set/get params by SlashOrSpacePath addressing",
+         "handle BackSpace send manually from terminal",
+         ],
+        ["SerialServer values",
+         "as Callable",
+         "ValueUnit",
+         "ValueVariants",
+         "list_results",
+         ],
+        ["SerialServer cmd",
+         "NONE is equivalent for SUCCESS",
+         "no need params (like line_parsed as before)",
+         "help - for show all variants (Units/Variants/Callables)!"
+         ]
     ]
 
     # HISTORY -----------------------------------------------
