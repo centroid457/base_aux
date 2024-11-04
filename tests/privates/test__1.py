412b7798e4f2085a190bba47704135a237dae6ff
admin <023>
2024-11-04 14:08:38 +0300

    [privates] separate env

--- tests/privates/test__1.py
+++ tests/privates/test__1.py
@@ -20,57 +20,6 @@ from base_aux.privates import (
 
 
 # =====================================================================================================================
-class Test__Env:
-    VALUE: str = "VALUE"
-    NAME_Exists: str = "Exists"
-    NAME_NotExists: str = "NotExists"
-
-    @classmethod
-    def setup_class(cls):
-        while cls.NAME_Exists in os.environ:
-            cls.NAME_Exists = f"{cls.NAME_Exists}_"
-
-        while cls.NAME_NotExists in os.environ:
-            cls.NAME_NotExists = f"{cls.NAME_NotExists}_"
-
-        os.environ[cls.NAME_Exists] = cls.VALUE
-
-        print()
-        print()
-        print(f"{cls.NAME_Exists=}")
-        print(f"{cls.NAME_NotExists=}")
-        print()
-        print()
-
-    @classmethod
-    def teardown_class(cls):
-        del os.environ[cls.NAME_Exists]
-
-    def setup_method(self, method):
-        self.Victim = PrivateEnv
-        self.Victim._RAISE = True
-
-    # -----------------------------------------------------------------------------------------------------------------
-    def test__Exists(self):
-        assert self.Victim()[self.NAME_Exists] == self.VALUE
-        assert getattr(self.Victim(), self.NAME_Exists) == self.VALUE
-
-    def test__notExists(self):
-        try:
-            self.Victim()[self.NAME_NotExists]
-        except AttributeError:
-            return
-        else:
-            assert False
-
-    def test__show(self):
-        # uppercase - see docstring for method!
-        envs = self.Victim().get_dict(self.NAME_Exists)
-        print(envs)
-        assert envs.get(self.NAME_Exists.upper()) == self.VALUE
-
-
-# =====================================================================================================================
 class Test__Csv:
     VICTIM: Type[PrivateCsv] = type("Victim", (PrivateCsv,), {})
     VICTIM2_FILENAME: str = f"{PrivateCsv.FILENAME}2"
---
+++ tests/privates/test__env.py
@@ -0,0 +1,64 @@
+import os
+import pytest
+import pathlib
+import shutil
+from tempfile import TemporaryDirectory
+from typing import *
+import abc
+
+from base_aux.classes import Exx__AnnotNotDefined
+from base_aux.privates import PrivateEnv
+
+
+# =====================================================================================================================
+class Test__Env:
+    VALUE: str = "VALUE"
+    NAME_Exists: str = "Exists"
+    NAME_NotExists: str = "NotExists"
+
+    @classmethod
+    def setup_class(cls):
+        while cls.NAME_Exists in os.environ:
+            cls.NAME_Exists = f"{cls.NAME_Exists}_"
+
+        while cls.NAME_NotExists in os.environ:
+            cls.NAME_NotExists = f"{cls.NAME_NotExists}_"
+
+        os.environ[cls.NAME_Exists] = cls.VALUE
+
+        print()
+        print()
+        print(f"{cls.NAME_Exists=}")
+        print(f"{cls.NAME_NotExists=}")
+        print()
+        print()
+
+    @classmethod
+    def teardown_class(cls):
+        del os.environ[cls.NAME_Exists]
+
+    def setup_method(self, method):
+        self.Victim = PrivateEnv
+        self.Victim._RAISE = True
+
+    # -----------------------------------------------------------------------------------------------------------------
+    def test__Exists(self):
+        assert self.Victim()[self.NAME_Exists] == self.VALUE
+        assert getattr(self.Victim(), self.NAME_Exists) == self.VALUE
+
+    def test__notExists(self):
+        try:
+            self.Victim()[self.NAME_NotExists]
+        except AttributeError:
+            return
+        else:
+            assert False
+
+    def test__show(self):
+        # uppercase - see docstring for method!
+        envs = self.Victim().get_dict(self.NAME_Exists)
+        print(envs)
+        assert envs.get(self.NAME_Exists.upper()) == self.VALUE
+
+
+# =====================================================================================================================
