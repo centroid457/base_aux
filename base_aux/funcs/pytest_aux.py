4995ca55ff5e4a6ba4015244b907ab11d62f7829
Andrei Starichenko <centroid@mail.ru>
2024-11-05 18:21:36 +0300

    [pytest] add example usage

--- base_aux/funcs/pytest_aux.py
+++ base_aux/funcs/pytest_aux.py
@@ -216,3 +216,31 @@ def test__expressions(expression):
 
 
 # =====================================================================================================================
+# @pytest.mark.parametrize(
+#     argnames="args, _EXPECTED",
+#     argvalues=[
+#         ((1, 1),        (True, True, False)),
+#         ((1, 2),        (False, False, True)),
+#         ((LAMBDA_TRUE, True), (False, False, True)),
+#
+#         ((ClsEq(1), 1), (True, True, False)),
+#         ((ClsEq(1), 2), (False, False, True)),
+#         ((1, ClsEq(1)), (True, True, False)),
+#         ((2, ClsEq(1)), (False, False, True)),
+#
+#         ((ClsEqExx(), 1), (Exception, False, True)),
+#         ((1, ClsEqExx()), (Exception, False, True)),
+#     ]
+# )
+# def test__compare_doublesided(args, _EXPECTED):
+#     func_link = Valid.compare_doublesided_or_exx
+#     pytest_func_tester__no_kwargs(func_link, args, _EXPECTED[0])
+#
+#     func_link = Valid.compare_doublesided__bool
+#     pytest_func_tester__no_kwargs(func_link, args, _EXPECTED[1])
+#
+#     func_link = Valid.compare_doublesided__reverse
+#     pytest_func_tester__no_kwargs(func_link, args, _EXPECTED[2])
+
+
+# =====================================================================================================================
