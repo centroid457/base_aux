9072a5c7a9e6a506fb30315d0cbe8ef9e4f2fb28
Andrei Starichenko <centroid@mail.ru>
2024-11-14 18:30:08 +0300

    [LambdaSleep] try add tests - need finish

--- tests/classes/test__lambdas.py
+++ tests/classes/test__lambdas.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 
 from base_aux.classes.lambdas import *
 from base_aux.funcs import *
@@ -17,7 +18,9 @@ from base_aux.objects.primitives import *
         (INST_BOOL_RAISE, (1,2,), (INST_BOOL_RAISE, Exception, Exception, True, False)),
     ]
 )
-def test__LambdaBool(source, args, _EXPECTED):
+def test__Lambdas(source, args, _EXPECTED):
+    # for Cls, Expected in zip(, _EXPECTED):    # tis good idea but we cant see directly exact line!
+
     pytest_func_tester__no_args_kwargs(Lambda(source, *args), _EXPECTED[0])
 
     pytest_func_tester__no_args_kwargs(LambdaBool(source, *args), _EXPECTED[1])
@@ -25,6 +28,25 @@ def test__LambdaBool(source, args, _EXPECTED):
 
     pytest_func_tester__no_args_kwargs(LambdaTrySuccess(source, *args), _EXPECTED[3])
     pytest_func_tester__no_args_kwargs(LambdaTryFail(source, *args), _EXPECTED[4])
+
+
+# =====================================================================================================================
+# TODO: finish!
+# @pytest.mark.parametrize(
+#     argnames="source, sec, _EXPECTED",
+#     argvalues=[
+#         (1, (1, 2,), 1),
+#         (10, (1, 2,), (10, True, False, True, False)),
+#         (LAMBDA_TRUE, (1, 2,), (True, True, False, True, False)),
+#         (LAMBDA_RAISE, (1, 2,), (Exception, Exception, Exception, False, True)),
+#         (INST_CALL_RAISE, (1, 2,), (Exception, Exception, Exception, False, True)),
+#         (INST_BOOL_RAISE, (1, 2,), (INST_BOOL_RAISE, Exception, Exception, True, False)),
+#     ]
+# )
+# def test__LambdaSleep(source, sec, _EXPECTED):
+#     pass
+#
+#     pytest_func_tester__no_args_kwargs(LambdaSleep(source, *args), _EXPECTED[0])
 
 
 # =====================================================================================================================
