42e43d10a6abd8cbf9828cd5af2f820653aa6406
Andrei Starichenko <centroid@mail.ru>
2024-11-19 17:28:40 +0300

    ready v0.0.20

--- PROJECT.py
+++ PROJECT.py
@@ -62,9 +62,9 @@ class PROJECT(PROJECT_BASE):
     FIXME: list[str] = [
     ]
     # -----------------------------------------------------------------------------------------------------------------
-    VERSION: tuple[int, int, int] = (0, 0, 19)
+    VERSION: tuple[int, int, int] = (0, 0, 20)
     NEWS: list[str] = [
-        "zeor ref +add old modules PrjFiles",
+        "[ValueUnit] fix cmp with ValueVariants",
     ]
 
 
