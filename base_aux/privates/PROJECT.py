86aed9da5a7ace6221b4a140ff3d71bcbfec1cbe
Andrei Starichenko <centroid@mail.ru>
2024-11-21 17:31:01 +0300

    zero ref all PRG.py

--- base_aux/privates/PROJECT.py
+++ base_aux/privates/PROJECT.py
@@ -1,44 +1,8 @@
 from typing import *
-from _aux__release_files import release_files_update
 
 
 # =====================================================================================================================
-# VERSION = (0, 0, 3)   # 1/deprecate _VERSION_TEMPLATE from PRJ object +2/place update_prj here in __main__ +3/separate finalize attrs
-# VERSION = (0, 0, 4)   # add AUTHOR_NICKNAME_GITHUB for badges
-VERSION = (0, 0, 5)     # separate PROJECT_BASE #TODO: need to separate into module!
-
-
-# =====================================================================================================================
-class PROJECT_BASE:
-    NAME_IMPORT: str
-    VERSION: tuple[int, int, int]
-
-    # AUTHOR ------------------------------------------------
-    AUTHOR_NAME: str = "Andrei Starichenko"
-    AUTHOR_EMAIL: str = "centroid@mail.ru"
-    AUTHOR_HOMEPAGE: str = "https://github.com/centroid457/"
-    AUTHOR_NICKNAME_GITHUB: str = "centroid457"
-
-    # AUX ----------------------------------------------------
-    CLASSIFIERS_TOPICS_ADD: list[str] = [
-        # "Topic :: Communications",
-        # "Topic :: Communications :: Email",
-    ]
-
-    # FINALIZE -----------------------------------------------
-    @classmethod
-    @property
-    def VERSION_STR(cls) -> str:
-        return ".".join(map(str, cls.VERSION))
-
-    @classmethod
-    @property
-    def NAME_INSTALL(cls) -> str:
-        return cls.NAME_IMPORT.replace("_", "-")
-
-
-# =====================================================================================================================
-class PROJECT(PROJECT_BASE):
+class PROJECT:
     # PROJECT ----------------------------------------------
     NAME_IMPORT: str = "private_values"
     KEYWORDS: list[str] = [
@@ -92,11 +56,6 @@ class PROJECT(PROJECT_BASE):
     NEWS: list[str] = [
         "[pypi.init] add static =NOW IT IS NOT WORKING!!!"
     ]
-
-
-# =====================================================================================================================
-if __name__ == '__main__':
-    release_files_update(PROJECT)
 
 
 # =====================================================================================================================
--- base_aux/requirements/PROJECT.py
+++ base_aux/requirements/PROJECT.py
@@ -1,44 +1,8 @@
 from typing import *
-from _aux__release_files import release_files_update
 
 
 # =====================================================================================================================
-# VERSION = (0, 0, 3)   # 1/deprecate _VERSION_TEMPLATE from PRJ object +2/place update_prj here in __main__ +3/separate finalize attrs
-# VERSION = (0, 0, 4)   # add AUTHOR_NICKNAME_GITHUB for badges
-VERSION = (0, 0, 5)     # separate PROJECT_BASE #TODO: need to separate into module!
-
-
-# =====================================================================================================================
-class PROJECT_BASE:
-    NAME_IMPORT: str
-    VERSION: tuple[int, int, int]
-
-    # AUTHOR ------------------------------------------------
-    AUTHOR_NAME: str = "Andrei Starichenko"
-    AUTHOR_EMAIL: str = "centroid@mail.ru"
-    AUTHOR_HOMEPAGE: str = "https://github.com/centroid457/"
-    AUTHOR_NICKNAME_GITHUB: str = "centroid457"
-
-    # AUX ----------------------------------------------------
-    CLASSIFIERS_TOPICS_ADD: list[str] = [
-        # "Topic :: Communications",
-        # "Topic :: Communications :: Email",
-    ]
-
-    # FINALIZE -----------------------------------------------
-    @classmethod
-    @property
-    def VERSION_STR(cls) -> str:
-        return ".".join(map(str, cls.VERSION))
-
-    @classmethod
-    @property
-    def NAME_INSTALL(cls) -> str:
-        return cls.NAME_IMPORT.replace("_", "-")
-
-
-# =====================================================================================================================
-class PROJECT(PROJECT_BASE):
+class PROJECT:
     # PROJECT ----------------------------------------------
     NAME_IMPORT: str = "requirements_checker"
     KEYWORDS: list[str] = [
@@ -81,11 +45,6 @@ designed for check requirements (systemOs) and raise/bool if no match
     NEWS: list[str] = [
         "[Pkg] add check_prj_installed_latest +apply in upgrade_prj+share",
     ]
-
-
-# =====================================================================================================================
-if __name__ == '__main__':
-    release_files_update(PROJECT)
 
 
 # =====================================================================================================================
