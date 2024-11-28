2458d11cdaf6e3e43369b0d13c4a9bbe6de25d36
Andrei Starichenko <centroid@mail.ru>
2024-11-28 15:56:52 +0300

    zero

--- base_aux/classes/attr_0_init_kwargs.py
+++ base_aux/classes/attr_0_init_kwargs.py
@@ -4,6 +4,7 @@ from typing import *
 # =====================================================================================================================
 class AttrInitKwargs:
     def __init__(self, *args, **kwargs):
+        super().__init__(*args)
         for name, value in kwargs.items():
             setattr(self, name, value)
 
