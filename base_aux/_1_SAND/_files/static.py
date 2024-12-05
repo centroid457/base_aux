cc4b5db7693da5cfd6950811f2dc16f1d1448f3a
Andrei Starichenko <centroid@mail.ru>
2024-12-05 11:05:12 +0300

    [git] ref to class

---
+++ base_aux/_1_SAND/_files/static.py
@@ -0,0 +1,5 @@
+from typing import *
+import pathlib
+
+
+TYPE__PATH = Union[str, pathlib.Path]
--- base_aux/systems/sys_git.py
+++ base_aux/systems/sys_git.py
@@ -2,53 +2,114 @@ import pathlib
 from base_aux.classes import Text
 
 try:
-    import git  # need try statement! if not installed git.exe raise Exx even if module was setup!!!
+    import git  # GITPYTHON # need try statement! if not installed git.exe raise Exx even if module was setup!!!
 except:
-    pass
+    msg = f"[ERROR] git - is not setup in OS"
+    print(msg)
 
 
 # =====================================================================================================================
-def git_get_last_commit_short_info_str(source_path=None):    # starichenko
+class Git:
     """
-    return last commit short info
-    for git repository found in passed path
+    GOAL
+    ----
+    get last commit short info instead of hard-version
     """
-    git_obj = None
+    # settings -------------
+    PATH: pathlib.Path | str = None
 
-    if not source_path:
-        source_path = pathlib.Path.cwd()
+    # aux -------------
+    REPO: git.Repo = None   # real object/only existed
 
-    try:
-        git_obj = git.Repo(source_path)
-    except Exception as exx:
-        print(f"{exx!r}")
-
-    if git_obj:
-        committer = git_obj.head.object.committer
+    # -----------------------------------------------------------------------------------------------------------------
+    def __init__(self, path: pathlib.Path | str = None):
+        if path:
+            self.PATH = pathlib.Path(path)
+        else:
+            self.PATH = pathlib.Path.cwd()
 
         try:
-            branch = git_obj.active_branch.name
-            branch = Text(source=branch).shortcut(maxlen=15)
+            self.REPO = git.Repo("../..")   # FIXME: need auto find ROOT!!!
+            self.REPO = git.Repo(self.PATH)
         except Exception as exx:
-            msg = f"GIT DETACHED HEAD - you work not on last commit on brange! {exx!r}"
-            print(msg)
-            branch = "*DETACHED_HEAD*"
+            print(f"git.Repo={exx!r}")
+            print(f"возможно GIT не установлен")
 
-        summary = git_obj.commit().summary
-        summary = Text(source=summary).shortcut(maxlen=15)
+    def check_ready(self) -> bool:
+        """
+        GOAL
+        ----
+        check if all ready to work
+        - git setup
+        - repo created
+        """
+        pass
 
-        hexsha = git_obj.head.object.hexsha[0:8]
+    def check_status(self) -> bool:
+        """
+        GOAL
+        ----
+        check if you work in validated repo! no changes from last commit
+        """
+        pass
 
-        committed_datetime = str(git_obj.head.object.committed_datetime)[0:19]
+    # -----------------------------------------------------------------------------------------------------------------
+    @property
+    def COMMITTER(self) -> str | None:
+        if self.REPO:
+            return self.REPO.head.object.committer
 
-        result = f"{branch}/{summary}/{committer}/{hexsha}/{committed_datetime}"
+    @property
+    def BRANCH(self) -> str | None:
+        if self.REPO:
+            try:
+                result = self.REPO.active_branch.name
+            except Exception as exx:
+                msg = f"[GIT] DETACHED HEAD - you work not on last commit on brange! {exx!r}"
+                print(msg)
+                result = "*DETACHED_HEAD*"
+            return result
 
-    else:
-        result = f"возможно GIT не установлен"
+    @property
+    def SUMMARY(self) -> str | None:
+        if self.REPO:
+            return self.REPO.commit().summary
 
-    git_mark = f"[git_mark//{result}]"
-    print(f"{git_mark=}")
-    return git_mark
+    @property
+    def HEXSHA(self) -> str | None:
+        if self.REPO:
+            return self.REPO.head.object.hexsha
+
+    @property
+    def DATETIME(self) -> str | None:
+        if self.REPO:
+            return self.REPO.head.object.committed_datetime
+
+    # -----------------------------------------------------------------------------------------------------------------
+    def mark_str(self) -> str:
+        """
+        EXAMPLE
+        -------
+        git_mark='[git_mark//main/zero/Andrei Starichenko/ce5c3148/2024-12-04 18:39:10]'
+        """
+        if self.REPO:
+            branch = Text(source=self.BRANCH).shortcut(maxlen=15)
+            summary = Text(source=self.SUMMARY).shortcut(maxlen=15)
+            dt = str(self.DATETIME)[0:19]
+
+            result = f"{branch}/{summary}/{self.COMMITTER}/{self.HEXSHA[0:8]}/{dt}"
+
+        else:
+            result = f"возможно GIT не установлен"
+
+        git_mark = f"[git_mark//{result}]"
+        print(f"{git_mark=}")
+        return git_mark
+
+
+# =====================================================================================================================
+if __name__ == '__main__':
+    Git().mark_str()
 
 
 # =====================================================================================================================
