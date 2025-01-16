e90364a7a02dace3efe03150c70c21be172d7619
Andrei Starichenko <centroid@mail.ru>
2025-01-16 13:58:55 +0300

    [root.__init__] add FOR import all bases

---
+++ base_aux/__init__.py
@@ -0,0 +1,13 @@
+"""
+GOAL
+----
+keep all base imports for quick work
+    from base_aux.base_imports import *
+
+REASON
+------
+try to resolve curcular imports
+just an attempt!
+"""
+
+from base_enums import *
