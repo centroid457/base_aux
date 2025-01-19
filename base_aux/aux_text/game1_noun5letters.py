d01b45f0845feaa23307b641a6caa3ba299a1edc
Andrei Starichenko <centroid@mail.ru>
2025-01-19 16:31:07 +0300

    [game_nouns_5letters] add words

--- base_aux/aux_text/game1_noun5letters.py
+++ base_aux/aux_text/game1_noun5letters.py
@@ -15,6 +15,16 @@ WORDS source
 https://github.com/cwaterloo/5bukv/blob/main/russian5.txt   -5letters for exact game
 https://gist.github.com/kissarat/bd30c324439cee668f0ac76732d6c825   -all counts (notFull!!!)
 
+
+NOTE
+----
+how to find new words
+
+1/ MANUALLY
+find combinations
+split groupes by first letter
+check by this code
+
 """
 
 import pathlib
@@ -26,28 +36,8 @@ text = file.read_text(encoding="utf8").lower()
 words: set[str] = set(text.split())
 
 lacks_source: str = """
-АБОРТ
-АВТОР
-АГЕНТ
-АДРЕС
-АКРИЛ
-АКТЕР
-АКТИВ
-АКУЛА
-АЛИБИ
-АЛМАЗ
-АЛЬФА
-АНГЕЛ
-АНИМЕ
-АРБУЗ
-АРЕНА
-АРМИЯ
-АРХИВ
-АСКОТ
-АТАКА
-АТЛАС
-АУДИТ
-АШРАМ
+ЯГОДА
+ЯГУАР
 """.lower()
 
 lacks_words = lacks_source.split()
