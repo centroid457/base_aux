9430421925321e47278ad779e329a165680d1080
Andrei Starichenko <centroid@mail.ru>
2025-01-23 15:28:31 +0300

    [_EqValidator] fix initions

--- base_aux/valid/m5_validator_0_base.py
+++ base_aux/valid/m5_validator_0_base.py
@@ -8,7 +8,7 @@ from base_aux.aux_callable.m1_callable_aux import *
 
 
 # =====================================================================================================================
-class _EqValidator:
+class _EqValidator(ArgsKwargs):
     """
     NOTE
     ----
@@ -24,18 +24,20 @@ class _EqValidator:
     -----
     PLACE ONLY IN FIRST PLACE!
     """
-    VALIDATOR: TYPE__VALID_VALIDATOR = True
+    VALIDATOR: TYPE__VALID_VALIDATOR
 
     ARGS: TYPE__ARGS_FINAL
     KWARGS: TYPE__KWARGS_FINAL
 
     OTHER_RAISED: bool = None
 
-    def __init__(self, validator: TYPE__VALID_VALIDATOR = None, args: TYPE__ARGS_DRAFT = (), kwargs: TYPE__KWARGS_DRAFT = None) -> None:
+    def __init__(self, validator: TYPE__VALID_VALIDATOR = None, *args, **kwargs) -> None:
         if validator is not None:
             self.VALIDATOR = validator
-        self.ARGS = ArgsKwargsAux(args).resolve_args()
-        self.KWARGS = ArgsKwargsAux(kwargs).resolve_kwargs()
+
+        # super(ArgsKwargs, self).__init__(*args, **kwargs)
+        self.ARGS = args
+        self.KWARGS = kwargs
 
     def __eq__(self, other) -> bool:
         return self.validate(other)
@@ -64,8 +66,11 @@ class _EqValidator:
             self.OTHER_RAISED = True
             other_result = exx
 
-        result = CallableAux(self.VALIDATOR).resolve_bool(*(other_result, *self.ARGS), **self.KWARGS)
+        result = CallableAux(self.VALIDATOR).resolve_bool(other_result, *self.ARGS, **self.KWARGS)
         return result
+
+    def VALIDATOR(self, other, *args, **kwargs) -> bool | NoReturn:
+        return NotImplemented
 
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -78,16 +83,17 @@ def _explore_1():
 
 # =====================================================================================================================
 class _EqValid_Base(_EqValidator):
-    def __init__(self, args=(), kwargs=None):
-        super().__init__(validator=None, args=args, kwargs=kwargs)
+    def __init__(self, *args, **kwargs):
+        # print(args, kwargs)
+        # super(ArgsKwargs, self).__init__(*args, **kwargs)     # not working!
 
-    def VALIDATOR(self, other, *args, **kwargs) -> bool | NoReturn:
-        return NotImplemented
-
+        # super().__init__(*args, **kwargs)
+        self.ARGS = args
+        self.KWARGS = kwargs
 
 # ---------------------------------------------------------------------------------------------------------------------
 @final
-class EqValidVariants(_EqValid_Base):
+class EqValid_Variants(_EqValid_Base):
     def VALIDATOR(self, other, *args, **kwargs) -> bool | NoReturn:
         if other in args:
             return True
@@ -95,48 +101,90 @@ class EqValidVariants(_EqValid_Base):
             return False
 
 
-def _explore_2():
-    print(EqValidVariants([0, 1, 2]) == 1)
-    print(EqValidVariants([0, 1, 2]) != 3)
-    print(EqValidVariants([*"123"]) == "1")
-    print(EqValidVariants([*"123"]) != "false")
+def _explore_21():
+    print(EqValid_Variants(0, 1, 2) == 1)
+    print(EqValid_Variants(0, 1, 2) != 3)
+    print(EqValid_Variants(*"123") == "1")
+    print(EqValid_Variants(*"123") != "false")
+
+
+@final
+class EqValid_VariantsStrLow(_EqValid_Base):
+    def VALIDATOR(self, other, *args, **kwargs) -> bool | NoReturn:
+        other = str(other).lower()
+        args = (str(arg).lower() for arg in args)
+        if other in args:
+            return True
+        else:
+            return False
+
+
+def _explore_22():
+    print(EqValid_VariantsStrLow(*"ABC") == "A")
+    print(EqValid_VariantsStrLow(*"ABC") == "a")
+    print(EqValid_VariantsStrLow(*"ABC") != "d")
 
 
 # ---------------------------------------------------------------------------------------------------------------------
 @final
-class EqValidExx(_EqValid_Base):
+class EqValid_Exx(_EqValid_Base):
     def VALIDATOR(self, other, *args, **kwargs) -> bool | NoReturn:
         return TypeAux(other).check__exception()
 
 
 def _explore_3():
-    print(EqValidExx() != False)
-    print(EqValidExx() != 1)
-    print(EqValidExx() == Exception)
-    print(EqValidExx() == Exception())
-    print(EqValidExx() == LAMBDA_EXX)
-    print(EqValidExx() == LAMBDA_RAISE)
+    print(EqValid_Exx() != False)
+    print(EqValid_Exx() != 1)
+    print(EqValid_Exx() == Exception)
+    print(EqValid_Exx() == Exception())
+    print(EqValid_Exx() == LAMBDA_EXX)
+    print(EqValid_Exx() == LAMBDA_RAISE)
 
 
 # ---------------------------------------------------------------------------------------------------------------------
 @final
-class EqValidRaise(_EqValid_Base):
+class EqValid_Raise(_EqValid_Base):
     def VALIDATOR(self, other, *args, **kwargs) -> bool:
         return self.OTHER_RAISED
 
 
 def _explore_4():
-    print(EqValidRaise() != False)
-    print(EqValidRaise() != Exception)
-    print(EqValidRaise() != Exception())
-    print(EqValidRaise() == LAMBDA_RAISE)
+    print(EqValid_Raise() != False)
+    print(EqValid_Raise() != Exception)
+    print(EqValid_Raise() != Exception())
+    print(EqValid_Raise() == LAMBDA_RAISE)
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+@final
+class EqValid_GtLt(_EqValid_Base):
+    def VALIDATOR(self, other, low: float = None, high: float = None) -> bool | NoReturn:
+        result = True
+
+        # low ----------
+        if low is not None:
+            result &= low < other
+
+        # high ----------
+        if high is not None:
+            result &= other < high
+
+        # final ----------
+        return result
+
+def _explore_5():
+    print(EqValid_GtLt(1, 3) != 1)
+    print(EqValid_GtLt(1, 3) == 2)
+    print(EqValid_GtLt(1, 3) != 3)
 
 
 # =====================================================================================================================
 if __name__ == "__main__":
     _explore_1()
     print()
-    _explore_2()
+    _explore_21()
+    print()
+    _explore_22()
     print()
     _explore_3()
     print()
