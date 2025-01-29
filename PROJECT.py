bc74576c2e4af4ca9d5bc56bd2800a35db2121b0
Andrei Starichenko <centroid@mail.ru>
2025-01-29 19:55:43 +0300

    ready v0.2.2

--- PROJECT.py
+++ PROJECT.py
@@ -62,9 +62,9 @@ class PROJECT(PROJECT_BASE):
     FIXME: list[str] = [
     ]
     # -----------------------------------------------------------------------------------------------------------------
-    VERSION: tuple[int, int, int] = (0, 2, 1)
+    VERSION: tuple[int, int, int] = (0, 2, 2)
     NEWS: list[str] = [
-        "[EqValid] add EqValid_Startswith",
+        "[EqValid] add EqValid_Endswith +repr",
     ]
 
 
