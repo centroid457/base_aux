93ca90a27cfbde2b173c5c3482863a748517abdf
Andrei Starichenko <centroid@mail.ru>
2025-01-31 18:02:52 +0300

    [filepath] ref tests

--- tests/files/test__1_dirpath.py
+++ tests/files/test__1_dirpath.py
@@ -8,12 +8,15 @@ CWD = pathlib.Path().cwd()
 
 # =====================================================================================================================
 class Test_Dirpath:
-    def test__1(self):
+    def test__cwd(self):
         assert Resolve_DirPath().resolve() == CWD
+
         assert Resolve_DirPath(None).resolve() == CWD
 
         assert Resolve_DirPath("").resolve() != CWD
         assert Resolve_DirPath("").resolve() == pathlib.Path(".")
+
+        assert Resolve_DirPath("hello").resolve() == pathlib.Path("hello")
 
 
 # =====================================================================================================================
