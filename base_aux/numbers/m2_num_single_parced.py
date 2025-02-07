a4173add06d4ffe5694a960d47c1ae64cc2b2da7
Andrei Starichenko <centroid@mail.ru>
2025-02-07 15:21:04 +0300

    [Nums] add NumParsedSingle

--- base_aux/numbers/m2_num_single_parced.py
+++ base_aux/numbers/m2_num_single_parced.py
@@ -1,14 +1,41 @@
-import math
+from typing import *
 
 from base_aux.base_exceptions.m1_exceptions import *
-from base_aux.aux_types.m2_info import *
+from base_aux.aux_types.m0_types import *
 
-from base_aux.aux_eq.m0_cmp_inst import CmpInst
+from base_aux.base_resolver.m1_resolver import *
+from base_aux.base_inits.m1_source import *
+
+from base_aux.aux_text.m1_text_aux import *
 
 
 # =====================================================================================================================
-class NumParsedSingle(Re):
+class _NumParsedSingle(InitSource, CallResolve):
+    SOURCE: TYPES.NUMBER = None
+
+    _numtype: NumType = NumType.BOTH
+
+    def init_post(self) -> None | NoReturn:
+        self.SOURCE = TextAux(self.SOURCE).parse__single_number(self._numtype)
+
+    def resolve(self) -> TYPES.NUMBER:
+        return self.SOURCE
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+@final
+class NumParsedSingle(_NumParsedSingle):
     pass
+
+
+@final
+class NumParsedSingleInt(_NumParsedSingle):
+    _numtype: NumType = NumType.INT
+
+
+@final
+class NumParsedSingleFloat(_NumParsedSingle):
+    _numtype: NumType = NumType.FLOAT
 
 
 # =====================================================================================================================
