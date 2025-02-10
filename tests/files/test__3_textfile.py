14d70afb1f976e48fce6bf25176ccfdde9c41bbd
Andrei Starichenko <centroid@mail.ru>
2025-02-10 17:04:37 +0300

    [FileAux] improve tests

--- tests/files/test__3_textfile.py
+++ tests/files/test__3_textfile.py
@@ -1,29 +1,36 @@
 import pathlib
 from base_aux.path2_file.m1_filepath import *
 from base_aux.path2_file.m2_file import *
+from base_aux.path2_file.m3_filetext import *
 
 
 # =====================================================================================================================
 text_load = "text_load"
+filepath = Resolve_FilePath(nameext="victim.txt").resolve()
 
 
 # =====================================================================================================================
-class Test_TextFile:
-    def test__1(self):
-        filepath = Resolve_FilePath(nameext="victim.txt").resolve()
-        file = FileAux(filepath=filepath)
-        assert file.clear_file() is True
-        assert file.read__text() == ""
-        assert file.TEXT == ""
-        assert file.write__text(text_load) == 9
-        assert file.TEXT == text_load
-        assert file.delete_file() is True
+def test__File():
+    file = FileAux(filepath=filepath)
+    assert file.clear_file() is True
+    assert file.read__text() == ""
+    assert file.TEXT == ""
+    assert file.write__text(text_load) == 9
+    assert file.TEXT == text_load
+    assert file.delete_file() is True
 
-        assert file.append__lines(1, 2) == 2
-        assert file.TEXT == "1\n2"
-        assert file.append__lines(3, 4) == 2
-        assert file.TEXT == "1\n2\n3\n4"
-        assert file.delete_file() is True
+    assert file.append__lines(1, 2) == 2
+    assert file.TEXT == "1\n2"
+    assert file.append__lines(3, 4) == 2
+    assert file.TEXT == "1\n2\n3\n4"
+    assert file.delete_file() is True
+
+
+def test__textFile():
+    file = TextFile(filepath=filepath, text=123)
+    assert file.write__text() == 3
+    assert file.TEXT == "123"
+    assert file.delete_file() is True
 
 
 # =====================================================================================================================
