7a7da6667e81e7db4221c2d01e456e5f225ea01f
Andrei Starichenko <centroid@mail.ru>
2025-02-13 14:32:30 +0300

    ready v0.2.7

--- PROJECT.py
+++ PROJECT.py
@@ -62,9 +62,9 @@ class PROJECT(PROJECT_BASE):
     FIXME: list[str] = [
     ]
     # -----------------------------------------------------------------------------------------------------------------
-    VERSION: tuple[int, int, int] = (0, 2, 6)
+    VERSION: tuple[int, int, int] = (0, 2, 7)
     NEWS: list[str] = [
-        "[Valid] add ValidBreak",
+        "[ValidSleep] add skipLink",
     ]
 
 
