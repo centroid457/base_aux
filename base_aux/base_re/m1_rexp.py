54af04d8ccf97faf8b12d7c11474f7fa93535248
Andrei Starichenko <centroid@mail.ru>
2025-03-06 17:58:17 +0300

    [RExp] zero ref

--- base_aux/base_re/m1_rexp.py
+++ base_aux/base_re/m1_rexp.py
@@ -10,6 +10,10 @@ class RExp(Nest_AttrKit):
     GOAL
     ----
     simple pattern with all expected params
+
+    RULES
+    -----
+    default methods use only None! to be ensure what would be replaced!
     """
     PAT: str
     FLAGS: int | None = None
@@ -17,7 +21,26 @@ class RExp(Nest_AttrKit):
 
 
 # =====================================================================================================================
-TYPING__ATTEMTS = Iterable[str | RExp]
+TYPING__REXP_ATTEMTS = Iterable[str | RExp]
+
+
+# =====================================================================================================================
+if __name__ == "__main__":
+    try:
+        RExp()
+        assert False
+    except:
+        pass
+
+    assert RExp(111)[0] == 111
+
+    assert RExp(111)[1] == None
+    assert RExp(111, 222)[1] == 222
+
+    assert RExp(111)[2] == None
+    assert RExp(111, sub=333)[2] == 333
+    assert RExp(111, sub=333).SUB == 333
+    assert RExp(1, 2, 3, sub=333).sub == 333
 
 
 # =====================================================================================================================
