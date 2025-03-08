a9d561b07a13ab35b08bbf89e092880e69cfb23f
Andrei Starichenko <centroid@mail.ru>
2025-03-08 17:01:57 +0300

    [RExp] move tests in file

--- base_aux/aux_text/m5_re1_rexp.py
+++ base_aux/aux_text/m5_re1_rexp.py
@@ -1,5 +1,3 @@
-from typing import *
-
 from base_aux.aux_attr.m4_kits import *
 
 
@@ -23,25 +21,6 @@ class RExp(Nest_AttrKit):
 # =====================================================================================================================
 TYPING__REXP_DRAFT = str | RExp
 TYPING__REXPS_FINAL = Iterable[RExp]
-
-
-# =====================================================================================================================
-def test__rexp():
-    try:
-        RExp()
-        assert False
-    except:
-        pass
-
-    assert RExp(111)[0] == 111
-
-    assert RExp(111)[1] == None
-    assert RExp(111, 222)[1] == 222
-
-    assert RExp(111)[2] == None
-    assert RExp(111, sub=333)[2] == 333
-    assert RExp(111, sub=333).SUB == 333
-    assert RExp(1, 2, 3, sub=333).sub == 333
 
 
 # =====================================================================================================================
