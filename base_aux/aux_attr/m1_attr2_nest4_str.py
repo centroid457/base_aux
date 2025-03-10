e92c554d853aedc1ad66d857a96cf2b1a1039c79
Andrei Starichenko <centroid@mail.ru>
2025-03-10 15:45:20 +0300

    [NestStr] add NestStR_AttrsPattern

--- base_aux/aux_attr/m1_attr2_nest4_str.py
+++ base_aux/aux_attr/m1_attr2_nest4_str.py
@@ -59,9 +59,38 @@ class NestStR_AttrsNotHidden:
 
 
 # =====================================================================================================================
+pass
+pass
+pass
+
+class NestStR_AttrsPattern:
+    """
+    GOAL
+    ----
+    use pattern to make a special string for instance
+    """
+    NEST_STR__ATTR_PATTERN: str
+
+    def __str__(self):
+        return self.NEST_STR__ATTR_PATTERN.format(self)
+
+    def __repr__(self):
+        return str(self)
+
+
+# =====================================================================================================================
 def _examples() -> None:
     class Victim(NestStR_AttrsNotPrivate):
         A0: int
+        A1: int = 1
+
+    victim = Victim()
+    print(victim)
+
+
+    class Victim(NestStR_AttrsPattern):
+        NEST_STR__ATTR_PATTERN = "A0={0.A0}, A1={0.A1}"
+        A0: int = 0
         A1: int = 1
 
     victim = Victim()
