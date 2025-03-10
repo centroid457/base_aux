4753cda17c688380d531de3557eb8754f0ac633a
Andrei Starichenko <centroid@mail.ru>
2025-03-10 20:04:22 +0300

    [FormatedLine] finish +need tests

--- base_aux/aux_text/m7_format.py
+++ base_aux/aux_text/m7_format.py
@@ -12,14 +12,15 @@ from base_aux.base_inits.m3_nest_init_annots_attrs_by_kwargs import *
 from base_aux.aux_datetime.m1_datetime import *
 from base_aux.aux_attr.m4_dump import AttrDump
 from base_aux.aux_attr.m4_kits import *
+from base_aux.aux_text.m6_nest_repr_clsname_str import *
 
 
 # =====================================================================================================================
 class PatFormat:
-    FIND_GROUPS: str = r"\{([a-zA-Z]*)([^{}]*)\}"   # (key, format)
+    FIND_GROUPS: str = r"\{([_a-zA-Z][_a-zA-Z01-9]*)?([^{}]*)\}"   # (key, formatter)
 
 
-class FormatedLine(NestCall_Other):
+class FormatedLine(NestCall_Other, NestRepr__ClsName_SelfStr):
     """
     GOAL
     ----
@@ -30,7 +31,7 @@ class FormatedLine(NestCall_Other):
     part for Alert messages
     """
     PAT_FORMAT: str = ""    # FORMAT PATTERN
-    PAT_RE: str = r""       # RE PATTERN
+    # PAT_RE: str = r""       # RE PATTERN
     VALUES: AttrDump        # values set
 
     # -----------------------------------------------------------------------------------------------------------------
@@ -38,7 +39,7 @@ class FormatedLine(NestCall_Other):
         self.PAT_FORMAT = pat_format
 
         self.init__keys()
-        # self.sai_args_kwargs(*args, **kwargs)
+        self.init__values_args_kwargs(*args, **kwargs)
 
     def init__keys(self):
         result_dict = {}
@@ -48,20 +49,11 @@ class FormatedLine(NestCall_Other):
                 key = f"_{index}"
             result_dict.update({key: None})
 
-        self.VALUES = AnnotAttrAux.annots__append(**result_dict)
+        self.VALUES = AnnotAttrAux().annots__append(**result_dict)
 
     # -----------------------------------------------------------------------------------------------------------------
     def init__values_args_kwargs(self, *args, **kwargs) -> bool:
-        for index, value in enumerate(args):
-            self[index] = value
-
-        for key, value in kwargs.items():
-            self[key] = value
-
-        return True
-
-
-
+        return AnnotAttrAux(self.VALUES).sai__by_args_kwargs(*args, **kwargs)
 
     # def __getattr__(self, item: str): # NOTE: DONT USE ANY GSAI HERE!!!
     #     return self[item]
@@ -94,17 +86,50 @@ class FormatedLine(NestCall_Other):
     #     raise AttributeError(item)
 
     # -----------------------------------------------------------------------------------------------------------------
-    def __str__(self):
-        return self.PAT_FORMAT.format(self.VALUES)
+    def __str__(self) -> str:
+        result = str(self.PAT_FORMAT)
+        values = AnnotAttrAux(self.VALUES).dump_dict()
+        group_index = 0
+        while True:
+            match = re.search(PatFormat.FIND_GROUPS, result)
+            if not match:
+                break
 
-    def __repr__(self):
-        return str(self)
+            name, formatter = match.groups()
+            name = name or f"_{group_index}"
+            name_orig = IterAux(values).item__get_original(name)
+            value = values[name_orig]
+            if value is None:
+                value=""
+            value_formatter = "{" + formatter + "}"
+            value_formatted = value_formatter.format(value)
+
+            result = re.sub(PatFormat.FIND_GROUPS, value_formatted, result, count=1)
+
+            group_index += 1
+        return result
 
 
 # =====================================================================================================================
 if __name__ == "__main__":
     victim = FormatedLine("{}", 1)
     assert victim.VALUES._0 == 1
+
+    print("{}".format(1))
+    print(str(victim))
+    assert str(victim) == "1"
+
+
+    victim = FormatedLine("hello {name}={value}", 1, name="name", value="value")
+    # assert victim.VALUES._1 == 1
+    assert victim.VALUES.name == "name"
+    print(str(victim))
+    assert str(victim) == "hello name=value"
+
+    victim.VALUES.name = "name2"
+    assert victim.VALUES.name == "name2"
+    print(str(victim))
+    assert str(victim) == "hello name2=value"
 
 
 # =====================================================================================================================
