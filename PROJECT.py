448a559d4dc0e3ab933c0c766f307baae9118b1f
Andrei Starichenko <centroid@mail.ru>
2025-03-19 13:49:59 +0300

    zero

--- PROJECT.py
+++ PROJECT.py
@@ -33,15 +33,9 @@ class PROJECT(PROJECT_BASE):
     FIXME: list[str] = [
     ]
     # -----------------------------------------------------------------------------------------------------------------
-    VERSION = Version((0, 2, 22))
+    VERSION = Version((0, 2, 23))
     NEWS: list[str] = [
-        "[alerts] ref to use deque+fix tlgrm +add Qtimer",
-        "[mt] ref TimeSeries +move into nympyAux",
-        "[nest] add NestCall_MethodName",
-        "[lambda] ref to keep Result/Exx in object",
-        "[Lambda] add Lambdalist +nest in threadItem",
-        "[flake8] add exclude Deprecated+hidden dirs",
-        "[serial] ref addresses_dump__answers quick threading "
+        "[lambdas/Threads] move all into callables +some ref",
     ]
 
 
