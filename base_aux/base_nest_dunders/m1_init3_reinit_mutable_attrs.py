7bb8f7cd90776f7eb83dc1728816679d0d218c71
Andrei Starichenko <centroid@mail.ru>
2025-03-19 17:08:04 +0300

    zero resort tests +add test__reinit_mutables

--- base_aux/base_nest_dunders/m1_init3_reinit_mutable_attrs.py
+++ base_aux/base_nest_dunders/m1_init3_reinit_mutable_attrs.py
@@ -15,8 +15,40 @@ class NestInit_Mutable:
     use simple copy past method!
     """
     def __init__(self, *args, **kwargs) -> None:
-        AttrAux(self).reinit__mutable_values()  # keep on first step!!! reinit only classvalues!
+        AttrAux(self).reinit__mutable_values()  # keep on first step!!! reinit only selfValues! not classvalues!
         super().__init__(*args, **kwargs)
+
+
+# =====================================================================================================================
+def test__reinit_mutables():
+    # check common way ---------
+    CONST = {}
+    CONST2 = {1:1}
+
+    assert {} == {}
+    assert {} is not {}
+    assert CONST is CONST
+
+    assert {1:1} == {1:1}
+    assert {1:1} is not {1:1}
+    assert CONST2 is CONST2
+
+    # NEW WAY -------------------
+    class Victim:
+        LIST = []
+        SET = {}
+        DICT = {}
+
+    class Victim2(Victim, NestInit_Mutable):
+        pass
+
+    victim = Victim()
+    assert victim.LIST == Victim.LIST
+    assert victim.LIST is Victim.LIST
+
+    victim2 = Victim2()
+    assert victim2.LIST == Victim2.LIST
+    assert victim2.LIST is not Victim2.LIST
 
 
 # =====================================================================================================================
