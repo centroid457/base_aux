8462a612cb734a901ceaf009dec7e748080f194c
Andrei Starichenko <centroid@mail.ru>
2025-04-24 15:43:24 +0300

    [TPS] fix hv tests

--- base_aux/testplans/TESTPLANS/psu800/tc8_hv1_150_off.py
+++ base_aux/testplans/TESTPLANS/psu800/tc8_hv1_150_off.py
@@ -6,6 +6,10 @@ from .tc0__test_params import *
 # =====================================================================================================================
 class TestCase(TestParams_Base):
     ATC_VOUT: int = 150
+    PTB_SET_EXTON: bool = False
+    PTB_SET_HVON: bool = True
+    PTB_SET_PSON: bool = True
+
     _DESCRIPTION = "\nграница включения - нижняя (OFF)"
 
 
--- base_aux/testplans/TESTPLANS/psu800/tc8_hv4_260_off.py
+++ base_aux/testplans/TESTPLANS/psu800/tc8_hv4_260_off.py
@@ -6,6 +6,10 @@ from .tc0__test_params import *
 # =====================================================================================================================
 class TestCase(TestParams_Base):
     ATC_VOUT: int = 260
+    PTB_SET_EXTON: bool = False
+    PTB_SET_HVON: bool = True
+    PTB_SET_PSON: bool = True
+
     _DESCRIPTION = "\nграница включения - верхняя (OFF)"
 
 
