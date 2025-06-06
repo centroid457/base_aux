8ec7edf6b3b9e7fa8448360cde3a3f5440e6a179
Andrei Starichenko <centroid@mail.ru>
2025-06-06 16:05:38 +0300

    [EqArgs] separate tests + renames

--- base_aux/aux_eq/m1_eq_args.py
+++ base_aux/aux_eq/m1_eq_args.py
@@ -1,12 +1,7 @@
-from typing import *
-import pytest
-
 from base_aux.base_nest_dunders.m1_init0_args_kwargs import *
 from base_aux.base_nest_dunders.m3_calls import *
-from base_aux.aux_attr.m1_annot_attr1_aux import *
 from base_aux.base_statics.m2_exceptions import *
 from base_aux.base_statics.m3_primitives import *
-from base_aux.aux_expect.m1_expect_aux import ExpectAux
 
 
 # =====================================================================================================================
@@ -34,26 +29,6 @@ class EqArgs(NestInit_Args_Implicit, NestCall_Resolve):
                 return False
 
         return True
-
-
-# =====================================================================================================================
-@pytest.mark.parametrize(
-    argnames="args, _EXPECTED",
-    argvalues=[
-        ((), Exception),
-        ((1, ), Exception),
-        ((1, 1), True),
-        ((1, 1, 1, 1), True),
-
-        ((1, 1, 1, 11), False),
-        ((1, 1, 11, 1), False),
-        ((1, INST_EQ_RAISE), False),
-        ((1, INST_EQ_FALSE), False),
-        ((1, INST_EQ_TRUE), True),
-    ]
-)
-def test__1(args, _EXPECTED):
-    ExpectAux(EqArgs(*args)).check_assert(_EXPECTED)
 
 
 # =====================================================================================================================
