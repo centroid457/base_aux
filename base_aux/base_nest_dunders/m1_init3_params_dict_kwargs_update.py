e020727d6a74ca49411837da32811e543ee8dc02
Andrei Starichenko <centroid@mail.ru>
2025-07-28 18:13:42 +0300

    [Dest] add NestInit_ParamsDict_UpdateKwargs

---
+++ base_aux/base_nest_dunders/m1_init3_params_dict_kwargs_update.py
@@ -0,0 +1,34 @@
+from base_aux.aux_dict.m2_dict_ic import *
+
+
+# =====================================================================================================================
+class NestInit_ParamsDict_UpdateKwargs:
+    """
+    GOAL
+    ----
+    when we have PARAMS like DictIc-as defParams and want to update it by Kwargs on init!
+
+    SPECIALLY CREATED FOR
+    ---------------------
+    indics
+    """
+    PARAMS: DictIc_LockedKeys
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args)
+        self.PARAMS.update(**kwargs)
+
+
+# =====================================================================================================================
+if __name__ == "__main__":
+    pass
+
+    class Victim(NestInit_ParamsDict_UpdateKwargs):
+        PARAMS = DictIc_LockedKeys(attr1=1, attr2=2)
+
+    victim = Victim(attr2=22)
+    assert victim.PARAMS["attr1"] == 1
+    assert victim.PARAMS["attr2"] == 22
+
+
+# =====================================================================================================================
