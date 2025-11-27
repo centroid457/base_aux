0c09244abd6089e54b23a39c85d79a99740d7176
Andrei Starichenko <centroid@mail.ru>
2025-11-27 14:55:05 +0300

    [datetime.Uptime] add stop/restart

--- base_aux/aux_datetime/m2_uptime.py
+++ base_aux/aux_datetime/m2_uptime.py
@@ -12,13 +12,19 @@ class Uptime:
     ---------------------
     tests
     """
-    time_started: float
+    def __init__(self, accuracy: float = 0):
+        self.accuracy: float = accuracy
 
-    def __init__(self):
-        self.reset()
+        self.time_started: float = time.time()
+        self.time_stopped: float | None = None
 
-    def reset(self) -> None:
+    def restart(self) -> None:
         self.time_started = time.time()
+        self.time_stopped = None
+
+    def stop(self) -> float:
+        self.time_stopped = time.time()
+        return self.get()
 
     # -----------------------------------------------------------------------------------------------------------------
     def get(self) -> float:
@@ -27,13 +33,23 @@ class Uptime:
         ----
         return time passed from start (initial time)
         """
-        return time.time() - self.time_started
+        if self.time_stopped:
+            return self.time_stopped - self.time_started
+        else:
+            return time.time() - self.time_started
 
     # -----------------------------------------------------------------------------------------------------------------
-    def check_lt(self, other: float, accuracy: float = 0) -> bool: raise NotImplementedError()
-    def check_le(self, other: float, accuracy: float = 0) -> bool: raise NotImplementedError()
-    def check_gt(self, other: float, accuracy: float = 0) -> bool: raise NotImplementedError()
-    def check_ge(self, other: float, accuracy: float = 0) -> bool: raise NotImplementedError()
+    def check_lt(self, other: float, accuracy: float = 0) -> bool:
+        raise NotImplementedError()
+
+    def check_le(self, other: float, accuracy: float = 0) -> bool:
+        raise NotImplementedError()
+
+    def check_gt(self, other: float, accuracy: float = 0) -> bool:
+        raise NotImplementedError()
+
+    def check_ge(self, other: float, accuracy: float = 0) -> bool:
+        raise NotImplementedError()
 
     # TODO: add __cmp__/__eq__
 
