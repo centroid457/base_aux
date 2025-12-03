ff0e76b88bab2ab76cd20b32869fd4940bf8b0ad
Andrei Starichenko <centroid@mail.ru>
2025-12-03 17:45:39 +0300

    [Sleep] extend with aio_* methods

--- base_aux/base_sleep/m1_sleep.py
+++ base_aux/base_sleep/m1_sleep.py
@@ -1,5 +1,6 @@
 import time
 from typing import *
+import asyncio
 
 
 # =====================================================================================================================
@@ -10,17 +11,22 @@ class Sleep:
     ----
     just a primitive func for tests or other purpose!
     """
-    SEC: float = 1
+    DEF_SEC: float = 1
+    sec: float
 
     def __init__(self, sec: float = None):
         if sec is not None:
-            self.SEC = sec
+            self.sec = sec
+        else:
+            self.sec = self.DEF_SEC
 
+    # -----------------------------------------------------------------------------------------------------------------
     def echo(self, echo: Any = None, *args, **kwargs) -> Any:
-        time.sleep(self.SEC)
+        time.sleep(self.sec)
         return echo
 
     def NONE(self, *args, **kwargs) -> None:
+        # NOTE: why used uppercase!? - cause "raise" name here will not be appropriate!
         return self.echo(echo=None, *args, **kwargs)
 
     def TRUE(self, *args, **kwargs) -> bool:
@@ -33,7 +39,28 @@ class Sleep:
         return self.echo(echo=Exception("Sleep.EXC"), *args, **kwargs)
 
     def RAISE(self, *args, **kwargs) -> NoReturn:
-        time.sleep(self.SEC)
+        self.echo()
+        raise Exception("Sleep.RAISE")
+
+    # -----------------------------------------------------------------------------------------------------------------
+    async def aio_echo(self, echo: Any = None, *args, **kwargs) -> Any:
+        await asyncio.sleep(self.sec)
+        return echo
+
+    async def aio_NONE(self, *args, **kwargs) -> None:
+        return await self.aio_echo(echo=None, *args, **kwargs)
+
+    async def aio_TRUE(self, *args, **kwargs) -> bool:
+        return await self.aio_echo(echo=True, *args, **kwargs)
+
+    async def aio_FALSE(self, *args, **kwargs) -> bool:
+        return await self.aio_echo(echo=False, *args, **kwargs)
+
+    async def aio_EXC(self, *args, **kwargs) -> Exception:
+        return await self.aio_echo(echo=Exception("Sleep.EXC"), *args, **kwargs)
+
+    async def aio_RAISE(self, *args, **kwargs) -> NoReturn:
+        await self.aio_echo()
         raise Exception("Sleep.RAISE")
 
 
