35f65969f4fb589c88279e7857b2614f71cc630d
Andrei Starichenko <centroid@mail.ru>
2026-01-21 15:12:33 +0300

    [docker] add explore with mq pytesting

---
+++ base_aux/aux_docker/conftest.py
@@ -0,0 +1,41 @@
+import pika
+import pytest
+from testcontainers.rabbitmq import RabbitMqContainer
+
+import os
+os.environ["TESTCONTAINERS_RYUK_DISABLED"] = "true"
+
+
+# =====================================================================================================================
+@pytest.fixture(scope="session")    # scope=SESSION - одно подключение на все тесты одного запуска тестового окружения! экономит время на создание и подключение!!!
+def rabbitmq_container():
+    """Сессионная фикстура для контейнера RabbitMQ"""
+    with RabbitMqContainer("rabbitmq:3-management") as container:
+        yield container
+
+
+@pytest.fixture
+def rabbitmq_connection(rabbitmq_container):
+    """Фикстура подключения к RabbitMQ"""
+    connection_params = pika.ConnectionParameters(
+        host=rabbitmq_container.get_container_host_ip(),
+        port=rabbitmq_container.get_exposed_port(5672),
+        credentials=pika.PlainCredentials(
+            username=rabbitmq_container.username,
+            password=rabbitmq_container.password
+        )
+    )
+    connection = pika.BlockingConnection(connection_params)
+    yield connection
+    connection.close()
+
+
+@pytest.fixture
+def rabbitmq_channel(rabbitmq_connection):
+    """Фикстура канала RabbitMQ"""
+    channel = rabbitmq_connection.channel()
+    yield channel
+    channel.close()
+
+
+# =====================================================================================================================
