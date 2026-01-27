f860370d4b1791057aa1fe4154ab2daa886cd5e7
Andrei Starichenko <centroid@mail.ru>
2026-01-27 17:56:30 +0300

    zero

--- base_aux/dockers/m1_docker.py
+++ base_aux/dockers/m1_docker.py
@@ -43,8 +43,61 @@ def docker__check_ready_os() -> bool:
 
 
 # =====================================================================================================================
+class Docker:
+    def __init__(self):
+        self.client = docker.from_env()
+        pass
+
+    def connect(self):
+        """
+        GOAL
+        ----
+        """
+
+    def run(self, image: str = "ubuntu:latest", cmd: str = "echo hello world", detach=True):
+        self.client.containers.run(image, cmd, detach=detach)
+        # <Container '45e6d2de7c54'>
+        print(f"{self.client=}")
+
+    def cont__list(self):
+        cont_list = self.client.containers.list()
+        print(f"{cont_list=}")
+        # [<Container '45e6d2de7c54'>, <Container 'db18e4f20eaa'>, ...]
+
+        if not cont_list:
+            return
+
+        try:
+            self.container = self.client.containers.get('45e6d2de7c54')
+            print(f"{self.container=}")
+        except:
+            return
+
+        cont_attrs = self.container.attrs['Config']['Image']  # "bfirsh/reticulate-splines"
+        print(f"{cont_attrs=}")
+
+        print(f"{self.container.logs()=}")
+        "Reticulating spline 1...\n"
+
+        self.container.stop()
+
+    def cont__stream_logs(self):
+        for line in self.container.logs(stream=True):
+            print(line.strip())
+
+    def images__list(self):
+        images_list = self.client.images.list()
+        # [<Image 'ubuntu'>, <Image 'nginx'>, ...]
+        print(f"{images_list=}")
+
+
+# =====================================================================================================================
 if __name__ == "__main__":
-    docker__check_ready_os()
+    assert docker__check_ready_os()
+
+    docker = Docker()
+    docker.run()
+    docker.cont__list()
 
 
 # =====================================================================================================================
