b61bd5327e98a6c2ba54c9ae2003baf9cfa0ab39
Andrei Starichenko <centroid@mail.ru>
2026-01-29 11:34:00 +0300

    [docker] fixes

--- base_aux/dockers/m1_docker.py
+++ base_aux/dockers/m1_docker.py
@@ -39,28 +39,102 @@ def docker__check_ready_os() -> bool:
         result = False
 
     print(f"docker__check_ready_os({result=})")
+    print()
     return result
 
 
 # =====================================================================================================================
-class Docker:
-    def __init__(self):
-        self.client = docker.from_env()
-        pass
+class DockerMan:
+    def __init__(
+            self,
+            image_name: str = "ubuntu:latest",
+            container_name: str = "test_container",
+    ):
+        self.image_name: str = image_name
+        self.container_name: str = container_name
 
-    def connect(self):
+        self._client: docker.client.DockerClient | None = None
+        self.container = None
+
+    def connect(self) -> bool:
         """
         GOAL
         ----
+        create connection to the system docker service-daemon!
         """
+        result = True
+        try:
+            self._client = docker.from_env()     # connect to daemon!
+        except BaseException as exc:
+            print(f"{exc!r}")
+            result = False
 
-    def run(self, image: str = "ubuntu:latest", cmd: str = "echo hello world", detach=True):
-        self.client.containers.run(image, cmd, detach=detach)
-        # <Container '45e6d2de7c54'>
-        print(f"{self.client=}")
+        print(f"connect/{result=}")
+        return True
 
+    def run_img(self) -> bool:
+        """
+        GOAL
+        ----
+        run image - create container
+        """
+        result = True
+
+        # check old/existed
+        try:
+            old_container = self._client.containers.get(self.container_name)
+            print(f"old exists: {old_container.id=}")
+
+            if old_container.status == "running":
+                print("old.stopping...")
+                old_container.stop()
+                print("old.stopped...")
+
+            print("old.deleting...")
+            old_container.remove()
+            print("old.deleted...")
+        except docker.errors.NotFound:
+            print("Старый контейнер не найден, создаю новый...")
+        except docker.errors.APIError as e:
+            print(f"Ошибка при удалении старого контейнера: {e}")
+
+        try:
+            self.container = self._client.containers.run(
+                image=self.image_name,
+                name=self.container_name,
+                # command=None,   # str/list
+                command="tail -f /dev/null",  # Бесконечная команда для поддержания работы иначе контейнер будет остановлен!!
+                detach=True,
+                remove=True,    # autoremove cont after stop
+            )
+            """
+            DETACH
+                if TRUE return ContainerCollection
+                FALSE - bytes (as cmd answer!)
+                bytes | docker.client.ContainerCollection
+                
+                detach=True/result=<Container: 153d16781ab9>    # OBJECT
+                detach=False/result=b'hello world\n'            # OUTPUT
+            """
+        except BaseException as exc:
+            print(f"{exc!r}")
+            result = False
+
+        print(f"run_img//{self.image_name=}/{self.container_name=}/{result=}")
+        return result
+
+    def send_cmd(
+            self,
+            cmd: str = "echo hello world",
+    ) -> str:
+        result = self.container.exec_run(cmd)
+        result = result.output.decode('utf-8') if result.output else ""
+        print(f"send_cmd//{cmd=}/{result=}")
+        return result
+
+    # ---------------------------------------------------------------------------------
     def cont__list(self):
-        cont_list = self.client.containers.list()
+        cont_list = self._client.containers.list()
         print(f"{cont_list=}")
         # [<Container '45e6d2de7c54'>, <Container 'db18e4f20eaa'>, ...]
 
@@ -68,7 +142,7 @@ class Docker:
             return
 
         try:
-            self.container = self.client.containers.get('45e6d2de7c54')
+            self.container = self._client.containers.get('45e6d2de7c54')
             print(f"{self.container=}")
         except:
             return
@@ -86,7 +160,7 @@ class Docker:
             print(line.strip())
 
     def images__list(self):
-        images_list = self.client.images.list()
+        images_list = self._client.images.list()
         # [<Image 'ubuntu'>, <Image 'nginx'>, ...]
         print(f"{images_list=}")
 
@@ -95,9 +169,28 @@ class Docker:
 if __name__ == "__main__":
     assert docker__check_ready_os()
 
-    docker = Docker()
-    docker.run()
-    docker.cont__list()
+    docker_man = DockerMan()
+    docker_man.connect()
+    docker_man.run_img()
+    print()
+    docker_man.send_cmd(f"echo hello 111")
+    docker_man.send_cmd(f"date")
+    docker_man.send_cmd(f"uname -a")
+    docker_man.send_cmd(f"ping localhost")
+    docker_man.send_cmd(f"echo hello 222")
+    docker_man.send_cmd(f"ping")
+    docker_man.send_cmd(f"ping ya.ru")
+
+    # docker_man.run(detach=False)
+
+    # result = docker_man.run(detach=True)
+    #
+    # print()
+    # ObjectInfo(result).print()
+    # print()
+    # ObjectInfo(result._client).print()
+
+    # docker_man.cont__list()
 
 
 # =====================================================================================================================
