317b72493c55847b75715c78150ff5a6cbc97674
Andrei Starichenko <centroid@mail.ru>
2026-04-02 14:07:22 +0300

    [websFront] create ero explore files js html/js

---
+++ base_aux/webs_front/d1_js_vanilla/js.js
@@ -0,0 +1,24 @@
+// экспорт массива
+export let months = ['Jan', 'Feb', 'Mar', 'Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// экспорт константы
+export const MODULES_BECAME_STANDARD_YEAR = 2015;
+
+// экспорт класса
+export class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+//function sayHi(user) {
+//  alert(`Hello, ${user}!`);
+//}
+//
+//function sayBye(user) {
+//  alert(`Bye, ${user}!`);
+//}
+//
+//export {sayHi, sayBye}; // список экспортируемых переменных
+
+//console.log(123)
